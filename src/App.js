bf0cc8d84f3d2dfd57c4eddf01d775f47b3dfed8
arcadag <piknik0711@yandex.ru>
2023-03-23 16:27:31 +0300

    example of using Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Contact from './Contact';
+import PageNotFound from './PageNotFound';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link>{' '}
+          <Link to="/contact">Contact</Link>{' '}
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="contact" element={<Contact/>}/>
+          <Route path="*" element={<PageNotFound/>}/>
+        </Routes>
+      </BrowserRouter>
+    
     </div>
   );
 }
